8c6abeaaf8724ba9fd89d53ae7eaa54a2b7086fa
mujahidur2004 <mujahidur2004@gmail.com>
2026-07-26 02:10:07 +0600

    Updated MainActivity, LoginActivity , RegisterActivity

--- app/src/main/java/com/example/campusconnect/LoginActivity.java
+++ app/src/main/java/com/example/campusconnect/LoginActivity.java
@@ -2,13 +2,16 @@ package com.example.campusconnect;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tvRegister;
+    TextView tvRegister, tvForgot;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,27 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         tvRegister = findViewById(R.id.tvRegister);
+        tvForgot = findViewById(R.id.tvForgot);
+        btnLogin = findViewById(R.id.btnLogin);
 
-        tvRegister.setOnClickListener(v -> {
+        tvRegister.setOnClickListener(this);
+        tvForgot.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tvRegister) {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
-        });
+
+        } else if (v.getId() == R.id.tvForgot) {
+            Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+            startActivity(intent);
+
+        } else if (v.getId() == R.id.btnLogin) {
+            Intent intent = new Intent(LoginActivity.this, HomeFeedActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/campusconnect/RegisterActivity.java
+++ app/src/main/java/com/example/campusconnect/RegisterActivity.java
@@ -2,13 +2,16 @@ package com.example.campusconnect;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-public class RegisterActivity extends AppCompatActivity {
+import android.widget.Toast;
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView btnlogin;
+    Button btnRegister;
 
     @Override
 
@@ -16,10 +19,26 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         btnlogin = findViewById(R.id.tvLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnlogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
 
-        btnlogin.setOnClickListener(v -> {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tvLogin) {
             Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
             startActivity(intent);
-        });
+        }
+        else if (v.getId() == R.id.btnRegister) {
+            Toast.makeText(this, "Registration Successful\nPlease Login", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
+
+
 }
